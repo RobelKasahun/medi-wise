faf699e7b8b517e0b3799aa3934b27de05218ac1
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-10-18 20:01:42 -0700

    Add Config file for the app setting

---
+++ backend/config.py
@@ -0,0 +1,15 @@
+# module that lets you interact with the operating system
+import os
+# load key-value pairs from a .env
+from dotenv import load_dotenv
+
+# Load environment variables
+loaded = load_dotenv()
+
+class Config:
+    # Get the Database URL
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # stop tracking every changes in objects
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Get Secret Key
+    SECRET_KEY=os.getenv('SECRET_KEY')
