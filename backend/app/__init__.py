b370327db21617c74178feccfe89451f1493b76a
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-10-19 17:47:47 -0700

    Establish connection between Flask-Backend and PostgrSQL database using SQLAlchemy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,47 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+from flask_mail import Mail
+from config import Config
 
 # open database connection
 db = SQLAlchemy()
 
+'''
+    enables database migrations, so when you change models, 
+    you can update the database schema smoothly.
+'''
+migrate = Migrate()
+
+# for password-reset emails
+mail = Mail()
+
 def create_app():
     app = Flask(__name__)
     
+    # load the configuration settings from the Config class
+    '''
+        load database uri, secret key, and track object modification
+    '''
+    app.config.from_object(Config)
+    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    
+    # for Reset password instructions link TODO:
+    
+    # initialization
+    mail.init_app(app)
+    # initialize db
+    db.init_app(app)
+    # initialize migration
+    migrate.init_app(app, db)
+    
     with app.app_context():
-        # Register the blueprints with the app
+        # import blueprints
         from app.routes.auth import auth_blueprint
         
+        # Register the blueprints with the app
         app.register_blueprint(auth_blueprint, url_prefix='/auth')
+        
     return app
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,11 +1,16 @@
-from flask import Blueprint
+from app.models.chat import Chat
+from app.models.user import User
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 
+# create auth blueprint
 auth_blueprint = Blueprint('auth', __name__)
 
 # API endpoint for Sign Up
 @auth_blueprint.route('/signup', methods=['GET', 'POST'])
 def register():
-    return f'Sign Up...'
+    return 'Sign up...'
 
 # API endpoint for login
 @auth_blueprint.route('/login', methods=['GET', 'POST'])
